211179e9bb2469f84160fa6d877e0fc51b5b59cf
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-12-25 16:49:49 -0300

    testando componente header

--- src/shared/Header.spec.tsx
+++ src/shared/Header.spec.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import { fireEvent } from "@testing-library/react";
+import React from "react";
+import { Header } from "./Header";
+
+jest.mock("./CartWidget", () => ({
+  CartWidget: () => <div>Cart widget</div>,
+}));
 
 describe("Header", () => {
-  it.todo("renders correctly")
-})
+  it("renders correctly", () => {
+    const { container } = renderWithRouter(() => <Header />);
+    expect(container.innerHTML).toMatch("Goblin Store");
+    expect(container.innerHTML).toMatch("Cart widget");
+  });
+
+  it("navigates to / on header title click", () => {
+    const { history, ...screen } = renderWithRouter(() => <Header />);
+    fireEvent.click(screen.getByText("Goblin Store"));
+    expect(history.location.pathname).toEqual("/");
+  });
+});
