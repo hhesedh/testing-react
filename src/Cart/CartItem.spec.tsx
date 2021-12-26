1d824740c456f28222cdf8a7048c88f4b9ca2d19
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-12-26 16:52:22 -0300

    implementação de testes em cartItem

--- src/Cart/CartItem.spec.tsx
+++ src/Cart/CartItem.spec.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { fireEvent, screen } from "@testing-library/react";
+import React from "react";
+import { Product } from "../shared/types";
+import { CartItem } from "./CartItem";
 
 describe("CartItem", () => {
-  it.todo("renders correctly")
+  const product: Product = {
+    name: "Product Foo",
+    price: 100,
+    image: "/image/source.png",
+  };
+  it("renders correctly", () => {
+    const { container } = renderWithRouter(() => (
+      <CartItem product={product} removeFromCart={() => {}} />
+    ));
 
-  describe("on 'Remove item' click", () => {
-    it.todo("calls passed in function")
-  })
-})
+    expect(container.innerHTML).toMatch("Product Foo");
+    expect(container.innerHTML).toMatch("100 Zm");
+    expect(screen.getByAltText("Product Foo")).toHaveAttribute(
+      "src",
+      "/image/source.png"
+    );
+  });
+  describe("on 'Remove' click", () => {
+    it("calls passed in function", () => {
+      const removeFromCartMock = jest.fn();
+
+      renderWithRouter(() => (
+        <CartItem product={product} removeFromCart={removeFromCartMock} />
+      ));
+
+      fireEvent.click(screen.getByText("Remove"));
+
+      expect(removeFromCartMock).toBeCalledWith(product);
+    });
+  });
+});
