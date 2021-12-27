128b4e1bcb6645a924d75ded7dcd69c47b6fa142
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-12-27 01:35:37 -0300

    implementação de testes em orderSummary

--- src/OrderSummary/OrderSummary.spec.tsx
+++ src/OrderSummary/OrderSummary.spec.tsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import { fireEvent, render, screen } from "@testing-library/react";
+import { OrderSummary } from "./OrderSummary";
+
+jest.mock("../shared/Loader", () => ({ Loader: () => <div>loader</div> }));
 
 describe("OrderSummary", () => {
+  afterEach(jest.clearAllMocks);
   describe("while order data being loaded", () => {
-    it.todo("renders loader")
-  }) 
+    it("renders loader", () => {
+      const stubUseOrder = () => ({
+        order: undefined,
+        isLoading: true,
+      });
 
+      render(<OrderSummary useOrderHook={stubUseOrder} />);
+
+      expect(screen.getByText("loader")).toBeInTheDocument();
+    });
+  });
   describe("when order is loaded", () => {
-    it.todo("renders order info")
-  }) 
+    const stubUseOrder = () => ({
+      isLoading: false,
+      order: {
+        products: [
+          {
+            name: "Product foo",
+            price: 10,
+            image: "image.png",
+          },
+        ],
+      },
+    });
+    it("renders order info", () => {
+      renderWithRouter(() => <OrderSummary useOrderHook={stubUseOrder} />);
+      expect(screen.getByText("Product foo")).toBeInTheDocument();
+    });
+    it("navigates to main page on button click", () => {
+      const { history } = renderWithRouter(() => (
+        <OrderSummary useOrderHook={stubUseOrder} />
+      ));
 
-  describe("with error", () => {
-    it.todo("renders error info")
-  }) 
-})
+      fireEvent.click(screen.getByText("Back to the store"));
+
+      expect(history.location.pathname).toEqual("/");
+    });
+  });
+  describe("without order", () => {
+    it("renders error message", () => {
+      const stubUseOrder = () => ({
+        isLoading: false,
+        order: undefined,
+      });
+
+      render(<OrderSummary useOrderHook={stubUseOrder} />);
+      expect(screen.getByText("Couldn't load order info.")).toBeInTheDocument();
+    });
+  });
+});
